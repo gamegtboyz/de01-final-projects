9df80d130ab5c6204f93a17f158876dde959f0d4
Asa Bejraputra <gamegame.gtboyz@gmail.com>
2024-12-06 00:59:34 +0100

    add fetch count and extend fetch scope

--- nyc-motor-collisions.py
+++ nyc-motor-collisions.py
@@ -7,9 +7,9 @@ token = "qMLbbltM4PxnOZ6W4yZgE3QIO"
 
 # define parameters
 params = {
-    '$limit' : 1000,
+    '$limit' : 100000,
     '$offset' : 0,
-    '$where' : 'crash_date >= "2024-01-01T00:00:00"',
+    '$where' : 'crash_date >= "2020-01-01T00:00:00"',
     '$order' : 'crash_date DESC',
 }
 
@@ -20,17 +20,21 @@ headers = {
 
 # fetch data
 all_data = []
+i = 0
 while True:
     response = requests.get(endpoints,headers = headers, params = params)
 
     if response.status_code == 200:
         data = response.json()
+
         # if there's no more data, then break it 
         if not data:
             break
 
         all_data.extend(data)
         params['$offset'] += len(data)
+        i += 1
+        print(f'fetching ({i})')
     else:
         print(f'Error: {response.status_code}, {response.text}')
         break
@@ -38,4 +42,4 @@ while True:
 # convert the downloaded data from json to DataFrame, then export it into .csv files
 df = pd.DataFrame(all_data)
 df.to_csv('nyc-collisions.csv',index = False)
-print("Dataset was downloaded successfully.")
+print(f"{i} rows of dataset were downloaded successfully.")
