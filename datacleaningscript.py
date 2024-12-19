3c7f02b724ec462131fc28f50bfa121b5c12f1f5
Asa Bejraputra <gamegame.gtboyz@gmail.com>
2024-12-19 20:52:43 +0100

    add drop with zero coordinates

--- datacleaningscript.py
+++ datacleaningscript.py
@@ -10,3 +10,6 @@ data.drop(['location'], axis = 1, inplace = True)
 
 # drop the null values in some columns
 data.dropna(subset = ['contributing_factor_vehicle_1','vehicle_type_code1','latitude','longitude'], inplace = True)
+
+# drop the observations with 0,0 coordinates
+data.drop(data[(data['latitude'] == 0) & (data['longitude'] == 0)].index, inplace = True)
